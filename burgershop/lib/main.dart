57b352cfbb9b5efdbc8c80fbe5ceaed9c213a42c
RoderickMS <Roderickxams@gmail.com>
2026-07-14 17:07:02 -0500

    ultimo cambio para poder conectar en arranque de la app con el firebase

--- burgershop/lib/main.dart
+++ burgershop/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:burgershop/screens/home/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:burgershop/screens/login/login_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -12,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
